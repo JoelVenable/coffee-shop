e99b6162b01464e1e67675c4b3e9720ce377919d
JoelVenable <30368779+JoelVenable@users.noreply.github.com>
2019-07-31 16:32:38 -0500

    Can fetch all resources, and fetch one by ID

---
+++ CoffeeShop/Controllers/CoffeesController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoffeesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public CoffeesController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Title, BeanType FROM Coffee";
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+                    List<Coffee> coffees = new List<Coffee>();
+
+                    while (await reader.ReadAsync())
+                    {
+                        Coffee coffee = new Coffee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            BeanType = reader.GetString(reader.GetOrdinal("BeanType"))
+                        };
+
+                        coffees.Add(coffee);
+                    }
+                    reader.Close();
+
+                    return Ok(coffees);
+                }
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    Coffee coffee = null;
+                    cmd.CommandText = @"SELECT Id, Title, BeanType FROM Coffee WHERE Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    if (await reader.ReadAsync())
+                    {
+                        coffee = new Coffee
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            BeanType = reader.GetString(reader.GetOrdinal("BeanType"))
+                        };
+
+                       
+                    }
+                    reader.Close();
+
+                    if (coffee == null) return NotFound($"Coffee with id {id} not found.");
+
+                    return Ok(coffee);
+                }
+            }
+        }
+
+    }
+}
